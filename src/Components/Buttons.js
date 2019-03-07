5d1ab0d0024b9331b107928905d9b71f1db37f75
EdiOancea <48124060+EdiOancea@users.noreply.github.com>
2019-03-07 15:37:43 +0200

    Implement buttons layout (#11)

---
+++ src/Components/Buttons.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Button from './Button';
+import styles from 'styled-components';
+
+const Buttons = () => {
+
+  const Box = styles.div`
+    width: 200px;
+    height: 250px;
+    overflow-y: auto;
+  `
+  return(
+    <Box>
+      <Button text={'AC'} color={'darkGray'}/>
+      <Button text={'+/-'} color={'darkGray'}/>
+      <Button text={'%'} color={'darkGray'}/>
+      <Button text={'÷'} color={'orange'}/>
+      <Button text={'7'}/>
+      <Button text={'8'}/>
+      <Button text={'9'}/>
+      <Button text={'x'} color={'orange'}/>
+      <Button text={'4'}/>
+      <Button text={'5'}/>
+      <Button text={'6'}/>
+      <Button text={'-'} color={'orange'}/>
+      <Button text={'1'}/>
+      <Button text={'2'}/>
+      <Button text={'3'}/>
+      <Button text={'+'} color={'orange'}/>
+      <Button text={'0'} size={'big'}/>
+      <Button text={','}/>
+      <Button text={'='} color={'orange'}/>
+    </Box>
+  );
+};
+
+export default Buttons;
